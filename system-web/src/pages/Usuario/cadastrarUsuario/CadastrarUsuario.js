040178919b94299e4c501d1946998a0809e6137c
leororato <leororato16@hotmail.com>
2024-10-23 22:25:31 -0300

    att

--- system-web/src/pages/Usuario/cadastrarUsuario/CadastrarUsuario.js
+++ system-web/src/pages/Usuario/cadastrarUsuario/CadastrarUsuario.js
@@ -6,10 +6,23 @@ import Text from "../../../components/Text";
 import Title from "../../../components/Title";
 import './CadastrarUsuario.css';
 import { useState } from "react";
+import api from "../../../axiosConfig";
+import SucessNotification from "../../../components/SucessNotification/SucessNotification";
+import ErrorNotification from "../../../components/ErrorNotification/ErrorNotification";
+import { useNavigate } from "react-router-dom";
 
 function CadastrarUsuario() {
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState("");
+    const [sucessMessage, setSucessMessage] = useState("");
 
     const [mostrarSenha, setMostrarSenha] = useState(false);
+    const [formDataUsuario, setFormDataUsuario] = useState({
+        nome: "",
+        login: "",
+        senha: "",
+        tipoAcesso: ""
+    });
 
     const handleToggleSenha = () => {
         if (mostrarSenha === true) {
@@ -19,9 +32,38 @@ function CadastrarUsuario() {
         }
     };
 
+    const cadastrarUsuario = async () => {
+        try {
+            await api.post('/usuario/cadastrar-usuario', formDataUsuario);
+
+            setTimeout(() => {
+                setSucessMessage("Usuário criado com sucesso");
+            }, 5000);
+
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || "Erro desconhecido ao criar usuário";
+                setErrorMessage(errorMessage);
+
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000);
+        }
+    }
+
+    const handleChangeFormData = (e) => {
+        const { name, value } = e.target;
+        setFormDataUsuario({
+            ...formDataUsuario,
+            [name]: value
+        })
+        console.log("valor: ", formDataUsuario)
+    }
+
     return (
         <div>
             <Header />
+            <ErrorNotification message={errorMessage} onClose={() => { setErrorMessage(null) }} />
+            <SucessNotification message={sucessMessage} onClose={() => { setSucessMessage(null) }} />
 
             <div className="container-cadastro-usuario">
 
@@ -31,11 +73,11 @@ function CadastrarUsuario() {
                         <div className="container-form-cadastro-usuario">
                             <div>
                                 <label>Nome:</label>
-                                <Input title={"Digite o nome do usuário"} placeholder={"Digite o nome do usuário"} type={"text"} />
+                                <Input title={"Digite o nome do usuário"} placeholder={"Digite o nome do usuário"} type={"text"} onChange={handleChangeFormData} name={"nome"} />
                             </div>
                             <div>
                                 <label>Login:</label>
-                                <Input title={"Digite o login do usuário"} placeholder={"Digite o login do usuário"} type={"text"} />
+                                <Input title={"Digite o login do usuário"} placeholder={"Digite o login do usuário"} type={"text"} onChange={handleChangeFormData} name={"login"} />
                             </div>
 
                             <div>
@@ -46,6 +88,8 @@ function CadastrarUsuario() {
                                         title={"Digite a senha do usuário"}
                                         placeholder={"Digite a senha do usuário"}
                                         className="input-password"
+                                        onChange={handleChangeFormData}
+                                        name={"senha"}
                                     />
                                     <Icon
                                         icon={mostrarSenha ? "mdi:eye-off" : "mdi:eye"}
@@ -55,11 +99,11 @@ function CadastrarUsuario() {
                                     />
                                 </div>
                             </div>
-                            
+
                             <div>
                                 <label>Tipo de acesso:</label>
-                                <select>
-                                    <option>Selecione</option>
+                                <select onChange={handleChangeFormData} name="tipoAcesso">
+                                    <option value={""}>Selecione</option>
                                     <option value={"A"}>Administrador</option>
                                     <option value={"G"}>Geral</option>
                                     <option value={"V"}>Visualização</option>
@@ -73,11 +117,13 @@ function CadastrarUsuario() {
                         <div id="botao-criar-usuario">
                             <Button
                                 text={"Cadastrar"}
+                                onClick={cadastrarUsuario}
                             />
                         </div>
                         <div id='botao-cancelar-criar-usuario'>
                             <Button
                                 text={"Cancelar"}
+                                onClick={() => {navigate("/inicio")}}
                             />
                         </div>
                     </div>
